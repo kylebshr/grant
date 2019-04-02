6c125ab69cc4db262705ab4c3347c4e59bd7bfc3
Kyle Bashour <kylebshr@me.com>
2019-04-02 16:48:05 -0700

    Only update during trading

--- Ticker/TickerPriceProvider.swift
+++ Ticker/TickerPriceProvider.swift
@@ -8,11 +8,13 @@ class TickerPriceProvider {
     private let update: Update
     private let url: URL
 
+    private var cached: Quote?
+
     init(ticker: String, apiKey: String, update: @escaping Update) {
         self.update = update
         self.url = URL(string: "https://cloud.iexapis.com/beta/stock/\(ticker)/quote?token=\(apiKey)")!
 
-        let timer = Timer.scheduledTimer(withTimeInterval: 6, repeats: true) { [weak self] _ in
+        let timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { [weak self] _ in
             self?.refreshIfNeeded()
         }
 
@@ -21,12 +23,40 @@ class TickerPriceProvider {
     }
 
     private func refreshIfNeeded() {
+        guard Date().isTradingHours || cached == nil else { return }
+
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             data.flatMap {
                 try? self.decoder.decode(Quote.self, from: $0)
             } .flatMap { quote in
-                DispatchQueue.main.async { self.update(quote) }
+                DispatchQueue.main.async {
+                    self.cached = quote
+                    self.update(quote)
+                }
             }
         }.resume()
+    }
+}
+
+extension Date {
+    var isTradingHours: Bool {
+        let calendar = Calendar.current
+        let est = convertedToEST()
+
+        guard !calendar.isDateInWeekend(est) else {
+            return false
+        }
+
+        let open = calendar.date(bySettingHour: 9, minute: 30, second: 0, of: est)!
+        let close = calendar.date(bySettingHour: 4, minute: 0, second: 0, of: est)!
+
+        return est >= open && est <= close
+    }
+
+    func convertedToEST() -> Date {
+        let timeZone = TimeZone(abbreviation: "EST")!
+        let targetOffset = TimeInterval(timeZone.secondsFromGMT())
+        let localOffset = TimeInterval(TimeZone.autoupdatingCurrent.secondsFromGMT(for: self))
+        return addingTimeInterval(targetOffset - localOffset)
     }
 }
